2aeaea2c872675ae91068b95d4a860e19940d3cd
Jason Walters <jason@lifeisguid.com>
2024-11-01 10:51:02 -0700

    Add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,77 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ProductModel } from './model/Products';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'psychedelic-pajamas';
+  
+  productForm: FormGroup = new FormGroup({});
+  productObj: ProductModel = new ProductModel();
+productList:  ProductModel[] = []; 
+
+  constructor(){
+    this.createForm();
+    const oldData = localStorage.getItem("ProdData")
+  if(oldData!=null){
+    const parseData = JSON.parse(oldData);
+  this.productList = parseData}
+  }
+
+  createForm() {
+  this.productForm = new FormGroup({
+    prodID: new FormControl(this.productObj.prodID),
+    productName: new FormControl(this.productObj.productName, [Validators.required]),
+    productDescription: new FormControl(this.productObj.productDescription),
+    productQuantity: new FormControl(this.productObj.productQuantity)
+  })
+}
+onSave(){
+  const oldData = localStorage.getItem("ProdData")
+  if(oldData!=null){
+    const parseData = JSON.parse(oldData);
+    this.productForm.controls['prodID'].setValue(parseData.length +1);
+    this.productList.unshift(this.productForm.value);
+  } else {
+    this.productList.unshift(this.productForm.value);
+    
+  }
+localStorage.setItem("ProdData", JSON.stringify(this.productList));
+this.reset();
+}
+
+onEdit(item: ProductModel){
+this.productObj = item;
+this.createForm()
+}
+
+onUpdate(){
+  const findRecord = this.productList.find(m=>m.prodID == this.productForm.controls['prodID'].value);
+  if(findRecord != undefined){
+    findRecord.productName = this.productForm.controls['productName'].value;
+    findRecord.productDescription = this.productForm.controls['productDescription'].value;
+    findRecord.productQuantity = this.productForm.controls['productQuantity'].value;
+  }
+  localStorage.setItem("ProdData", JSON.stringify(this.productList));
+this.reset();
+  }
+
+onDelete(id: number){
+ const toDelete = confirm("Are you sure you want to delete");
+ if(toDelete){
+const index = this.productList.findIndex(m=>m.prodID == id);
+this.productList.splice(index,1)
+localStorage.setItem("ProdData", JSON.stringify(this.productList));
+ }
+}
+reset(){
+this.productObj = new ProductModel();
+this.createForm();
+}
 }
---
+++ src/app/model/Products.ts
@@ -0,0 +1,16 @@
+export class ProductModel {
+prodID: number;
+productName: string;
+productDescription: string;
+productQuantity: number;
+
+constructor(){
+this.prodID = 0;
+this.productName = '';
+this.productDescription = '';
+this.productQuantity = 0;
+}
+
+
+
+}
